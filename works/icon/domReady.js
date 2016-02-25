694b3a85558a816371f701299f638c7de3e8bc07
zhuxiaona1009 <1217218718@qq.com>
2016-02-25 21:35:54 +0800

    xin

--- works/icon/domReady.js
+++ works/icon/domReady.js
@@ -1,7 +1,5 @@
-//版权 北京智能社©, 保留所有权利
 function $(fn){
-	
-	if(document.addEventListener){//高级
+	if(document.addEventListener){
 		document.addEventListener("DOMContentLoaded",fn,false);
 	} else {
 		document.attachEvent("onreadystatechange",function(){
